01d0870b32ba2644a26a68d151bf5f09acb7e091
Blake <btsmith1212@gmail.com>
2024-05-13 17:32:38 -0500

    managing state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
-  function handleSelect(selectedButton) {
-    console.log(selectedButton);
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {    
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
 }
   return (
     <div>
@@ -28,7 +32,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {selectedTopic}
         </section>
       </main>
     </div>
