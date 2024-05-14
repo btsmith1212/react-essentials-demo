201a8b21544942914a0a9e72c436dee304bbec34
Blake <btsmith1212@gmail.com>
2024-05-13 20:19:22 -0500

    conditional content

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,25 @@ import TabButton from './components/TabButton/TabButton.jsx';
 import { EXAMPLES } from './data.js';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {    
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
 }
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (              
+    <div id='tab-content'>
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+    );
+  }
   return (
     <div>
       <Header/>
@@ -33,15 +46,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
