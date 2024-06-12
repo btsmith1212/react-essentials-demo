bb4a9676113c1531a166dae979b31d31f48d8af5
Blake <btsmith1212@gmail.com>
2024-06-11 19:20:57 -0500

    working with multiple jsx slots

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { EXAMPLES } from "../../data";
 import TabButton from "../TabButton/TabButton";
 import Section from "../Section/Section";
+import Tabs from "../Tabs/Tabs";
 
 export default function Examples() {
     const [selectedTopic, setSelectedTopic] = useState();
@@ -26,14 +27,19 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-        <h2>{title}</h2>
-        <menu>
-          <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx'}onClick={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedTopic === 'props'}onClick={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state'}onClick={() => handleSelect('state')}>State</TabButton>
-        </menu>
-        {tabContent}
+          <Tabs 
+            buttons={       
+              <>   
+              <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+              <TabButton isSelected={selectedTopic === 'jsx'}onClick={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton isSelected={selectedTopic === 'props'}onClick={() => handleSelect('props')}>Props</TabButton>
+              <TabButton isSelected={selectedTopic === 'state'}onClick={() => handleSelect('state')}>State</TabButton>
+                </>
+              }
+            >
+              {tabContent}
+          </Tabs>
+        <menu></menu>
       </Section>
     );
 
